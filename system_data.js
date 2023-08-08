0ca7e9b82991e08b011bdc06aa95db76c83c0510
LucasFurioFranco <lucasfuriofranco@gmail.com>
2023-08-08 18:34:45 +0000

    GTM loading changes]

--- system_data.js
+++ system_data.js
@@ -1,1 +1,1 @@
-const sys_data = { "git_commit_id": "d2411fd", "name": "lf2_dev_br"};module.exports = sys_data;
+const sys_data = { "git_commit_id": "c833b56", "name": "lf2_dev_br"};module.exports = sys_data;
